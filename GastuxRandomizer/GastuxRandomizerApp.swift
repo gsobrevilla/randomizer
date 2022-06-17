ea2ef143e3fd8367aab41d469752fdbd4c64f1ac
Gastón Sobrevilla <gastonsobrevilla@gmail.com>
2022-06-17 21:31:28 +0100

    Initial Commit

---
+++ GastuxRandomizer/GastuxRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GastuxRandomizerApp.swift
+//  GastuxRandomizer
+//
+//  Created by Gastón on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GastuxRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
